ef50453a576452b25c9e6fcbaf5541a2511fe3ab
Tran Bao Phuc <felixtran2000@gmail.com>
2022-01-25 19:41:48 +0700

    image about path

--- pages/about/index.js
+++ pages/about/index.js
@@ -9,7 +9,7 @@ export default function About(){
     return <Page>
         <br/>
        
-        <Image width={100} height={100} src='/avatar.png'/>
+        <Image width={100} height={100} src='images/avatar.png'/>
         <p>Tran Bao Phuc (felixtr)</p>
         <p>UI/UX enthusiast, front-end enjoyer. This blog to share what I&apos;ve learned and discuss new ideas.</p>
     </Page>
